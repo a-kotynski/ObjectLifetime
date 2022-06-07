2fded991dca2bc491a98a862903b355f25df9087
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-07 09:48:37 +0200

    .

--- Program.cs
+++ Program.cs
@@ -11,6 +11,42 @@ namespace ObjectLifetime
         static void Main(string[] args)
         {
             Car myCar = new Car();
+
+            myCar.Make = "Oldmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            myOtherCar.Model = "98";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            myOtherCar = null;
+
+            /*
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            */
+
+            myCar = null;
+
+            Console.ReadLine();
         }
     }
     class Car
